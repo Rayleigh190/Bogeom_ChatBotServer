5b199adb470619e5994674d764a19a8537b15563
rayleigh <rayleigh190@gmail.com>
2023-06-05 19:44:31 +0900

    feat: 1차 기능구현 완료

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var api_config = require("./secrets.json");
+let api_config = require("./secrets.json");
+let item_data = require("./item_data.json");
 const apiKey = api_config.openAiApiKey;
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
@@ -12,7 +13,7 @@ const openai = new OpenAIApi(configuration);
 
 //CORS 이슈 해결
 // let corsOptions = {
-//   origin: "https://chatdoge123jocoding.pages.dev",
+//   origin: "",
 //   credentials: true,
 // };
 // app.use(cors(corsOptions));
@@ -21,61 +22,6 @@ app.use(cors());
 //POST 요청 받을 수 있게 만듬
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-let food_data = {
-  식품: [
-    {
-      name: "농심 안성탕면",
-      spicy: 2,
-      amount: 5,
-    },
-    {
-      name: "까르보 불닭볶음면",
-      spicy: 4,
-      amount: 4,
-    },
-    {
-      name: "불닭볶음면",
-      spicy: 5,
-      amount: 3,
-    },
-    {
-      name: "오뚜기 진라면 매운맛",
-      spicy: 3,
-      amount: 2,
-    },
-  ],
-};
-
-let daily_data = {
-  생활용품: [
-    {
-      name: "섬유탈취제 페브리즈 상퀘한향",
-      scent: 2,
-      stimulation: 5,
-    },
-    {
-      name: "섬유탈취제 페브리즈 은은한향",
-      scent: 4,
-      stimulation: 4,
-    },
-    {
-      name: "피죤 섬유탈취제 핑크로즈",
-      scent: 5,
-      stimulation: 3,
-    },
-    {
-      name: "해피바스 내추럴 정말 순한 바디워시 900g",
-      scent: 3,
-      stimulation: 2,
-    },
-    {
-      name: "해피바스 내추럴 정말 촉촉한 바디워시 900g",
-      scent: 3,
-      stimulation: 5,
-    },
-  ],
-};
 
 // POST method route
 app.post("/BGChat", async function (req, res) {
@@ -88,29 +34,26 @@ app.post("/BGChat", async function (req, res) {
   let messages = [
     {
       role: "system",
-      // content: JSON.stringify(food_data) + "이 데이터에서만 답변해줘.",
+      // content:
+      //   JSON.stringify(item_data.food_data).toString +
+      //   JSON.stringify(item_data.daily_data).toString +
+      //   "이 데이터에서만 답변해줘.",
       content:
-        `당신은 상품 추천 챗봇입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 보검이입니다. 당신은 주어진 데이터를 분석하고 질문에 대한 답을 줄 수 있습니다. 대신 다음 데이터에서만 명확히 답변해야 합니다.` +
-        JSON.stringify(food_data) +
-        JSON.stringify(daily_data),
+        `당신은 상품 추천 챗봇입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 보검이입니다. 당신은 주어진 데이터를 분석하고 이를 바탕으로 질문에 대한 답을 줄 수 있습니다. 대신 반드시 다음에 제공된 데이터 안에서만 명확하고 간결하게 한글로만 답변해야 합니다. 반드시 다음 데이터 안에서만 답변해야 합니다. 데이터 : ` +
+        JSON.stringify(item_data.food_data) +
+        JSON.stringify(item_data.daily_data),
     },
     {
       role: "system",
       content:
-        "amount의 범위는 1에서 5이고 5에 가까울 수록 양이 많은 겁니다. spicy의 범위는 1에서 5이고 5에 가까울 수록 매운 겁니다.",
+        "다음은 주어진 데이터에 대한 참고 정보야. amount의 범위는 1에서 5이고 5에 가까울 수록 양이 많은 겁니다. spicy의 범위는 1에서 5이고 5에 가까울 수록 매운 겁니다. review_rate의 범위는 1에서 5이고 5에 가까울 수록 평점이 좋은 것입니다. scent의 범위는 1에서 5이고 5에 가까울 수록 향이 좋은것 입니다. stimulation의 범위는 1에서 5이고 5에 가까울 수록 자극이 강한 겁니다. bubble의 범위는 1에서 5이고 5에 가까울 수록 거품이 많이 나는 겁니다.",
     },
     // {
     //   role: "assistant",
     //   content:
-    //     "안녕하세요! 저는 보검이입니다. 상품에 대한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다.",
-    // },
-    // {
-    //   role: "user",
-    //   content: `저의 생년월일과 태어난 시간은 ${myDateTime}입니다. 오늘은 ${todayDateTime}입니다.`,
-    // },
-    // {
-    //   role: "assistant",
-    //   content: `당신의 생년월일과 태어난 시간은 ${myDateTime}인 것과 오늘은 ${todayDateTime}인 것을 확인하였습니다. 운세에 대해서 어떤 것이든 물어보세요!`,
+    //     "제가 알고 있는 데이터는 다음과 같습니다." +
+    //     JSON.stringify(item_data.food_data).toString +
+    //     JSON.stringify(item_data.daily_data).toString,
     // },
   ];
 
@@ -143,7 +86,8 @@ app.post("/BGChat", async function (req, res) {
       completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: messages,
-        temperature: 0.7,
+        // max_tokens: 100,
+        temperature: 0.8, // 창의성
       });
       break;
     } catch (error) {
@@ -154,8 +98,12 @@ app.post("/BGChat", async function (req, res) {
       );
     }
   }
-
-  let fortune = completion.data.choices[0].message["content"];
+  let fortune;
+  if (retries < 3) {
+    fortune = completion.data.choices[0].message["content"];
+  } else {
+    fortune = "요청을 실패 했어요 ㅠㅅㅠ 새로고침 해주세요!";
+  }
   console.log(fortune);
 
   res.json({ assistant: fortune });
